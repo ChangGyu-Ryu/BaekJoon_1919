9167a0c21dcc9624335cd01264d1b982b2b74456
ChangGyu-Ryu <ckue21@gmail.com>
2023-11-16 22:58:51 +0900

    Baekjoon_1919

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,25 +3,27 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
+
+    public static int[] getAlphabatCount(String str) {
+        int[] count = new int[26];
+
+        for(int i = 0; i < str.length(); i++)
+            count[str.charAt(i) - 'a']++;
+
+        return count;
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
         String b = sc.next();
-        int[] countA = new int[26]; // bread [ 1, 1, 0, 1, 1, .. ]
-        int[] countB = new int[26]; // dared [ 1, 0, 0, 2, 1, .. ]
+        int[] countA = getAlphabatCount(a);      // bread [ 1, 1, 0, 1, 1, .. ]
+        int[] countB = getAlphabatCount(b);      // dared [ 1, 0, 0, 2, 1, .. ]
         int ans = 0;
 
-        for(int i = 0; i < a.length(); i++)
-            countA[a.charAt(i) - 'a']++;
-        for(int i = 0; i < b.length(); i++)
-            countB[b.charAt(i) - 'a']++;
-
         for(int i = 0; i < 26; i ++) {
-            if(countA[i] > countB[i])
-                ans += countA[i] - countB[i];
-            else if (countA[i] < countB[i])
-                ans += countB[i] - countA[i];
+            ans += Math.abs(countA[i] - countB[i]);
         }
 
         System.out.println(ans);
